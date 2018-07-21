728cf25d1877f1f8397d1f3c8c6a40672a6489fd
Zac Willmington <zac.willmington@gmail.com>
2018-07-21 18:18:21 +0000

    automatically backed up by learn

--- spec/controllers/admin/preferences_controller_spec.rb
+++ spec/controllers/admin/preferences_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin::PreferencesController, type: :controller do
   it "has an index page" do
+      binding.pry
     get :index
     expect(response).to be_ok
   end
